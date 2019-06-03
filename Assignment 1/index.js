58ebf6c7afb8395ecc9f7e0d191cc2cff807afc8
Dinakar Chappa <dchappa@gmail.com>
2019-06-03 15:29:41 -0400

    Add files via upload
    Removed the commented lines of code, and also fixed the eventListener issue.

--- Assignment 1/index.js
+++ Assignment 1/index.js
@@ -1,102 +1,3 @@
-// $( document ).ready(function() {
-//      $("select").change(function () {
-
-//     if ($('#blank').is(':selected')){
-//     	$('#text').html("");
-//     }
-
-// 	if ($('#dog').is(':selected')){
-//     	$('#text').html(dog.description);
-//     }
-
-//     if ($('#cat').is(':selected')){
-//     	$('#text').html(cat.description);
-//     }
-
-//     if ($('#rabbit').is(':selected')){
-//     	$('#text').html(rabbit.description);
-//     }
-
-//     if ($('#caterpillar').is(':selected')){
-//     	$('#text').html(caterpillar.description);
-//     }
-
-//     if ($('#peacock').is(':selected')){
-//     	$('#text').html(peacock.description);
-//     }
-
-//     if ($('#mouse').is(':selected')){
-//     	$('#text').html(mouse.description);
-//     }
-
-//     if ($('#python').is(':selected')){
-//     	$('#text').html(python.description);
-//     }
-
-//     if ($('#hamster').is(':selected')){
-//     	$('#text').html(hamster.description);
-//     }
-
-//     if ($('#frog').is(':selected')){
-//     	$('#text').html(frog.description);
-//     }
-
-//     if ($('#bee').is(':selected')){
-//     	$('#text').html(bee.description);
-//     }
-// 	})
-// });
-
-// var dog = {
-//     name: "dog",
-//     description: "An adorable animal loved by many. Kept as pets."
-// };
-
-// var cat = {
-//     name: "cat",
-//     description: "An incredibly lazy animal, but kept as pets for some reason."
-// };
-
-// var caterpillar = {
-//     name: "caterpillar",
-//     description: "A worm-like animal, transforms into a butterfly as it ages."
-// };
-
-// var rabbit = {
-//     name: "rabbit",
-//     description: "A small animal that loves to run around. Loves eating carrots."
-// };
-
-// var peacock = {
-//     name: "peacock",
-//     description: "Lucky bird, not only do people enjoy its beauty, it also doesn't seem to have a predator"
-// };
-
-// var mouse  ={
-//     name: "mouse",
-//     description: "Small, squeaky animal. Contrary to Tom and Jerry, it's often falls victim to the cat as prey"
-// };
-
-// var python = {
-//     name: "python",
-//     description: "Slithery animal, very dangerous. If you're a programmer, then it might be worth looking into."
-// };
-
-// var hamster = {
-//     name: "hamster",
-//     description: "Small, adorable pet. Unfortunately it dies rather quickly."
-// };
-
-// var frog = {
-//     name: "frog",
-//     description: "Small amphibian animal, has unique jump and very long tongue"
-// };
-
-// var bee = {
-//     name: "bee",
-//     description: "Most likely to not be(e) your best friend. But the fate of the earth does depend on it."
-// };
-
 class Animal {
     constructor(name, description){
         this.name = name;
@@ -130,10 +31,6 @@ document.getElementsByTagName('div')[0].appendChild(text);
 var list = document.getElementById("animalList");
 div.appendChild(list);
 
-// var blank = document.createElement('option');
-// blank.appendChild( document.createTextNode('') );
-// blank.id = 'blank';
-// list.appendChild(blank);
 
 for(i=0; i < animals.length; i++){
     var new_animal = document.createElement('option');
@@ -147,58 +44,7 @@ var list = document.getElementById("animalList")
 list.addEventListener("change", function(){
     for(i=0; i < animals.length; i++){
         if(list.value == animals[i].name){
-            document.getElementById("text").html = animals[i].description
+            document.getElementById("text").innerHTML = animals[i].description
         }
     }
 })
-
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(dog.name) );
-// animal.id = 'dog';
-// list.appendChild(animal);
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(cat.name) );
-// animal.id = 'cat';
-// list.appendChild(animal);
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(caterpillar.name) );
-// animal.id = 'caterpillar';
-// list.appendChild(animal);
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(rabbit.name) );
-// animal.id = 'rabbit';
-// list.appendChild(animal);
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(peacock.name) );
-// animal.id = 'peacock';
-// list.appendChild(animal);
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(mouse.name) );
-// animal.id = 'mouse';
-// list.appendChild(animal);
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(python.name) );
-// animal.id = 'python';
-// list.appendChild(animal);
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(hamster.name) );
-// animal.id = 'hamster';
-// list.appendChild(animal);
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(frog.name) );
-// animal.id = 'frog';
-// list.appendChild(animal);
-
-// var animal = document.createElement('option');
-// animal.appendChild( document.createTextNode(bee.name) );
-// animal.id = 'bee';
-// list.appendChild(animal);
