bc9d98afa4bcdd8bac78818729dc9e0ab0f7168b
Dinakar Chappa <dchappa@gmail.com>
2019-06-11 09:44:24 -0400

    Making small edits so that every element is dynamically created

--- Assignment 3_New/public/js/animalSelect.js
+++ Assignment 3_New/public/js/animalSelect.js
@@ -14,6 +14,13 @@ blank.appendChild( document.createTextNode('') );
 blank.id = 'blank';
 list.appendChild(blank);
 
+for(var i in animals){
+    let new_animal = document.createElement('option');
+    new_animal.appendChild( document.createTextNode(animals[i].name) );
+    new_animal.id = animals[i].name;
+    list.appendChild(new_animal);
+}
+
 var list = document.getElementById("animalList")
 list.addEventListener("change", function(){
             document.getElementById("text").innerHTML = document.getElementById("animalList").value
