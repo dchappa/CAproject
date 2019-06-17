190ccf4a7db8c4ea269e90075896ab75022a74fd
Dinakar Chappa <dchappa@gmail.com>
2019-06-17 09:01:50 -0400

    Working on the delete functionality

--- Assignment_3.5/public/js/animalTable.js
+++ Assignment_3.5/public/js/animalTable.js
@@ -1,4 +1,5 @@
 var table = document.createElement('table')
+table.id = "aniTable"
 var row = document.createElement('tr')
 var animalName = document.createElement('th');
 animalName.innerHTML = "Name"
@@ -9,14 +10,37 @@ row.appendChild(animalDescription)
 table.appendChild(row)
 
 for(var i in animals){
-var aniRow = document.createElement('tr')
-  var tdName = document.createElement('td');
-  tdName.innerHTML = animals[i].name
-  aniRow.appendChild(tdName)
-  var tdDescription = document.createElement('td');
-  tdDescription.innerHTML = animals[i].description
-  animalDescription.appendChild(tdDescription)
-  aniRow.appendChild(tdDescription)
-  table.appendChild(aniRow)
+let aniRow = document.createElement('tr');
+  let tdName = document.createElement('td');
+  tdName.innerHTML = animals[i].name;
+  aniRow.appendChild(tdName);
+  let tdDescription = document.createElement('td');
+  tdDescription.innerHTML = animals[i].description;
+  animalDescription.appendChild(tdDescription);
+  aniRow.appendChild(tdDescription);
+  var deleteBtn = document.createElement("button");
+	deleteBtn.id = "deleteBtn";
+	deleteBtn.style.height = "25px";
+	deleteBtn.style.width = "50px";
+	deleteBtn.innerHTML = "Delete"; 
+	var editBtn = document.createElement("button");
+	editBtn.id = "editBtn";
+	editBtn.style.height = "25px";
+	editBtn.style.width = "50px";
+	editBtn.innerHTML = "Edit";  
+  tdDescription.appendChild(deleteBtn);
+  tdDescription.appendChild(editBtn);
+  table.appendChild(aniRow);
 }
-document.body.appendChild(table)
+
+deleteBtn.onclick = function(){
+	xhttp.open("DELETE", "http://localhost:3000/animals", true);
+	xhttp.setRequestHeader('Content-Type', 'application/json')
+	let data = '{"name":"' + animalEntry.value + '", "description":"' + descriptionEntry.value + '"}'
+	xhttp.send(data);
+}
+
+
+
+
+document.body.appendChild(table);
--- Assignment_3.5/public/js/newAnimal.js
+++ Assignment_3.5/public/js/newAnimal.js
@@ -1,21 +1,44 @@
 var xhttp = new XMLHttpRequest();
 
 var addBtn = document.createElement("button");
+addBtn.id = "addBtn";
 addBtn.style.height = "25px";
 addBtn.style.width = "50px";
 addBtn.innerHTML = "Add";
 
 var animalEntry = document.createElement("INPUT");
+animalEntry.id = "animalEntry"
 animalEntry.setAttribute("type", "text");
 animalEntry.value = "Animal Name";
 
 var descriptionEntry = document.createElement("INPUT");
+descriptionEntry.id = "descriptionEntry"
 descriptionEntry.setAttribute("type", "text");
 descriptionEntry.value = "Description";
 
 addBtn.onclick = function(){
 	xhttp.open("POST", "http://localhost:3000/animals", true);
-	xhttp.send("name=" + animalEntry.value + "&description=" + descriptionEntry.value);
+	xhttp.setRequestHeader('Content-Type', 'application/json')
+	let data = '{"name":"' + animalEntry.value + '", "description":"' + descriptionEntry.value + '"}'
+	xhttp.send(data);
+	// This appends it to the table
+	table = document.getElementById("aniTable");
+	let aniRow = document.createElement('tr');
+  	let tdName = document.createElement('td');
+  	tdName.innerHTML = animalEntry.value;
+  	aniRow.appendChild(tdName);
+  	let tdDescription = document.createElement('td');
+  	tdDescription.innerHTML = descriptionEntry.value;
+  	aniRow.appendChild(tdDescription);
+  	table.appendChild(aniRow);
+
+  	// This will append it to the dropdown list
+  	list = document.getElementById("animalList");
+  	let new_animal = document.createElement('option');
+    new_animal.appendChild( document.createTextNode(animalEntry.value));
+    new_animal.id = animalEntry.value;
+    new_animal.value = descriptionEntry.value;
+    list.appendChild(new_animal);
 }
 
 var body = document.getElementsByTagName("body")[0];
