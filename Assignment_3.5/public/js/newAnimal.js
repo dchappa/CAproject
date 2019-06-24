375286cc05dfe16ae2780538dbde1cc287d50344
Dinakar Chappa <dchappa@gmail.com>
2019-06-24 15:55:03 -0400

    Edit button still has issues, but functionality is working again!

--- Assignment_3.5/public/js/newAnimal.js
+++ Assignment_3.5/public/js/newAnimal.js
@@ -1,8 +1,5 @@
 var xhttp = new XMLHttpRequest();
 
-var currID = parseInt(i) + 1;
-var newID = 1;
-
 var addBtn = document.createElement("button");
 	addBtn.id = "addBtn";
 	addBtn.style.height = "25px";
@@ -30,27 +27,43 @@ addBtn.onclick = function(){
 				const value = JSON.parse(this.responseText);
 				animals.push(value);
 				var deleteBtn = document.createElement("button");
-		deleteBtn.style.height = "25px";
-		deleteBtn.style.width = "50px";
-		deleteBtn.innerHTML = "Delete"; 
+				deleteBtn.style.height = "25px";
+				deleteBtn.style.width = "50px";
+				deleteBtn.innerHTML = "Delete"; 
 
-	var editBtn = document.createElement("button");
-		editBtn.style.height = "25px";
-		editBtn.style.width = "50px";
-		editBtn.innerHTML = "Edit";
-		var table = document.getElementById("aniTable");
+				var editBtn = document.createElement("button");
+				editBtn.style.height = "25px";
+				editBtn.style.width = "50px";
+				editBtn.innerHTML = "Edit";
+
+	
+			addAnimal(animalEntry.value, descriptionEntry.value, deleteBtn, editBtn);
+			
+		// 	for(let i in animals){
+		// 	deleteButtonListener(i);
+		// 	editButtonListener(i);
+		// }
+			} 
+		};
+		var data = '{"name":"' + animalEntry.value + '", "description":"' + descriptionEntry.value + '"}';
+
+		xhttp.send(data);
+
+		
+	}
+
+function addAnimal(name, description, deleteBtn, editBtn){
+	var table = document.getElementById("aniTable");
 		let aniRow = document.createElement('tr');
-	  	var tdName = document.createElement('td');
-	  	tdName.innerHTML = animalEntry.value;
-	  	tdName.id = animals[currID]._id + "name"
+	  	let tdName = document.createElement('td');
+	  	tdName.innerHTML = name;
+	  	tdName.id = animals[animals.length-1]._id + "name"
 	  	aniRow.appendChild(tdName);
 	  	var tdDescription = document.createElement('td');
-	  	tdDescription.innerHTML = descriptionEntry.value;
-	  	tdDescription.id = animals[currID]._id + "description"
-	  	deleteBtn.id = "delete" + (currID);
-	  	editBtn.id = "edit" + (currID);
-	  	currID = currID + 1;
-	  	newID = newID + 1;
+	  	tdDescription.innerHTML = description;
+	  	tdDescription.id = animals[animals.length-1]._id + "description"
+	  	deleteBtn.id = "delete" + (animals.length-1);
+	  	editBtn.id = "edit" + (animals.length-1);
 	  	tdDescription.appendChild(deleteBtn);
 	  	tdDescription.appendChild(editBtn);
 	  	aniRow.appendChild(tdDescription);
@@ -58,79 +71,64 @@ addBtn.onclick = function(){
 	  	// This will append it to the dropdown list
 	  	list = document.getElementById("animalList");
 	  	let new_animal = document.createElement('option');
-	    new_animal.appendChild( document.createTextNode(animalEntry.value));
-	    new_animal.id = animalEntry.value;
-	    new_animal.value = descriptionEntry.value;
+	    new_animal.appendChild( document.createTextNode(name));
+	    new_animal.id = name;
+	    new_animal.value = description;
 	    list.appendChild(new_animal);
-
-			} 
-		};
-		var data = '{"name":"' + animalEntry.value + '", "description":"' + descriptionEntry.value + '"}';
-
-		xhttp.send(data);
-
-	
-
-	  	// This will append it to the dropdown list
-	  	// list = document.getElementById("animalList");
-	  	// let new_animal = document.createElement('option');
-	   //  new_animal.appendChild( document.createTextNode(animalEntry.value));
-	   //  new_animal.id = animalEntry.value;
-	   //  new_animal.value = descriptionEntry.value;
-	   //  list.appendChild(new_animal);
-
-		for(let i in animals){
-			xhttpDel = new XMLHttpRequest();
-			let currButton = document.getElementById("delete" + i);
+	    deleteButtonListener(animals.length-1);
+		editButtonListener(animals.length-1);
+}
+function deleteButtonListener(aniIndex){
+	xhttpDel = new XMLHttpRequest();
+			let currButton = document.getElementById("delete" + aniIndex);
 			currButton.addEventListener("click", function(){
-					xhttpDel.open("DELETE", "http://localhost:3000/animals/" + animals[i]._id, true);
+					xhttpDel.open("DELETE", "http://localhost:3000/animals/" + animals[aniIndex]._id, true);
 						xhttpDel.onload = function () {
 						var animalList = JSON.parse(xhttpDel.responseText);
 						if (xhttpDel.readyState == 4 && xhttpDel.status == "200") {
-							console.table(animalList);
+								//deletes from the dropdown
+							 	list = document.getElementById("animalList");
+							  	list.remove(aniIndex+1);
+							//deletes from the table
+							document.getElementById(animals[aniIndex]._id + "name").style.display = "none";
+							document.getElementById(animals[aniIndex]._id + "description").style.display = "none";
 						} else {
 							console.error(animalList);
 						}
 					}
-					xhttpDel.send();
-					//deletes from the dropdown
-					  	list = document.getElementById("animalList");
-					  	list.remove((parseInt(i)+1));
-					//deletes from the table
-					document.getElementById(animals[i]._id + "name").style.display = "none";
-					document.getElementById(animals[i]._id + "description").style.display = "none";
-					
-				});
-
-			currButton = document.getElementById("edit" + i);
+					xhttpDel.send();					 			
+				})
+		}
+editButtonListener = function(aniIndex){
+		let currButton = document.getElementById("edit" + aniIndex);
 			let clicked = false;
 			var confirmBtn = document.createElement("button");
 			currButton.addEventListener("click", function(){
 			if(!clicked){
 				xhttpEdit = new XMLHttpRequest();
 				clicked = true;
-				let row = document.getElementById(animals[i]._id + "description");
+				let row = document.getElementById(animals[aniIndex]._id + "description");
 
 				var editName = document.createElement("INPUT");
 				editName.id = "editName"
 				editName.setAttribute("type", "text");
-				editName.value = animals[i].name;
+				editName.value = animals[aniIndex].name;
 
 				var editDescription = document.createElement("INPUT");
 				editDescription.id = "editDescription"
 				editDescription.setAttribute("type", "text");
-				editDescription.value = animals[i].description;
+				editDescription.value = animals[aniIndex].description;
 
 				row.appendChild(editName);
 				row.appendChild(editDescription);
 
-				confirmBtn.id = "confirm" + i;
+				confirmBtn.id = "confirm" + aniIndex;
 				confirmBtn.style.height = "25px";
 				confirmBtn.style.width = "60px";
 				confirmBtn.innerHTML = "Confirm"; 
 
 				confirmBtn.addEventListener("click", function(){
-					xhttpEdit.open("PUT", "http://localhost:3000/animals/" + animals[i]._id, true);
+					xhttpEdit.open("PUT", "http://localhost:3000/animals/" + animals[aniIndex]._id, true);
 					xhttpEdit.setRequestHeader('Content-type','application/json; charset=utf-8');
 							xhttpEdit.onload = function () {
 							var animalList = JSON.parse(xhttpEdit.responseText);
@@ -141,25 +139,23 @@ addBtn.onclick = function(){
 							}
 						}
 						let data = '{"name":"' + editName.value + '", "description":"' + editDescription.value + '"}'
-						let new_name = document.getElementById(animals[i]._id + "name");
+						let new_name = document.getElementById(animals[aniIndex]._id + "name");
 						new_name.innerHTML = editName.value;
-						let new_description = document.getElementById(animals[i]._id + "description");
+						let new_description = document.getElementById(animals[aniIndex]._id + "description");
 						new_description.innerHTML = editDescription.value;
 						xhttpEdit.send(data);
 						list = document.getElementById("animalList");
-						list.remove((parseInt(i)+1));
-						list.options[i].innerHTML = editName.value;
-				  		list.options[i].value = editDescription.value;
-				  		// deleteBtn.style.display = "block";
-				  		// editBtn.style.display = "block";
+						list.remove(aniIndex+1);
+						list.options[aniIndex].innerHTML = editName.value;
+				  		list.options[aniIndex].value = editDescription.value;
 				  		row.appendChild(deleteBtn);
 				  		row.appendChild(editBtn);
 					});
 				row.appendChild(confirmBtn);
 			}
 		});
-	}
 }
+	
 
 
 
