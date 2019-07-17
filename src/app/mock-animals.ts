ac5df0725964d2c334a060b46dd0f01acbe28c42
Dinakar Chappa <dchappa@gmail.com>
2019-07-17 09:04:35 -0400

    Delete mock-animals.ts

--- src/app/mock-animals.ts
+++
@@ -1,6 +0,0 @@
-import { Animal } from './animal/animal.model';
-
-export const ANIMALS: Animal[] = [
-  {name: 'Dog', color: 'Red', size: 'Medium', dob: '01/01/2001'},
-  {name: 'Cat', color: 'brown', size: 'Small', dob: '09/09/2009'}
-];
